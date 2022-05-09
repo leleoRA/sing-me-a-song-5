cb1dfc5f44f34644df514b756127405981e32c05
blarth <jminocente@gmail.com>
2022-05-09 15:24:19 -0300

    fix test router with test env

--- back-end/src/services/testService.ts
+++ back-end/src/services/testService.ts
@@ -1,6 +1,5 @@
 import { recommendationRepository } from "../repositories/recommendationRepository.js"
-import { faker } from '@faker-js/faker';
-import { CreateRecommendationData } from "./recommendationsService.js";
+
 
 export async function resetDatabase(){
     await recommendationRepository.truncate();
