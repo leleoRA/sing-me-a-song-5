c83828950c8b712b21593769328209c295c5ca9d
blarth <jminocente@gmail.com>
2022-05-09 15:52:28 -0300

    chores: prettier config added, migrate scripts added

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,21 +1,19 @@
-import cors from "cors";
-import express from "express";
-import "express-async-errors";
-import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-import recommendationRouter from "./routers/recommendationRouter.js";
-import testsRouter from "./routers/testRouter.js";
+import cors from 'cors'
+import express from 'express'
+import 'express-async-errors'
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js'
+import recommendationRouter from './routers/recommendationRouter.js'
+import testsRouter from './routers/testRouter.js'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.use("/recommendations", recommendationRouter);
-if (process.env.NODE_ENV === "tests") {
-  app.use(testsRouter);
+app.use('/recommendations', recommendationRouter)
+if (process.env.NODE_ENV === 'tests') {
+  app.use(testsRouter)
 }
 
+app.use(errorHandlerMiddleware)
 
-
-app.use(errorHandlerMiddleware);
-
-export default app;
+export default app
